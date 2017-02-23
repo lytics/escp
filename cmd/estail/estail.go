2466b093fac5baaa075605bf118e052348002849
eric <esniff@gmail.com>
2017-02-23 15:19:51 -0800

    estail works using esscroll now

--- cmd/estail/estail.go
+++ cmd/estail/estail.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
+	"context"
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"os"
-	"sort"
 	"strings"
 	"time"
 
+	"encoding/json"
+
+	"github.com/lytics/escp/esscroll"
 	log "github.com/lytics/escp/logging"
 )
 
@@ -20,72 +19,35 @@ func fatalf(msg string, args ...interface{}) {
 	os.Exit(2)
 }
 
-// StringArray implements flag.Value interface
-type StringArray []string
-
-func (a *StringArray) Set(s string) error {
-	msgs := strings.Split(s, ",")
-	*a = append(*a, msgs...)
-	return nil
-}
-
-func (a *StringArray) String() string {
-	return strings.Join(*a, ",")
-}
-
 func main() {
 	host := "localhost:9200"
-	indexPrefix := "logstash-"
-	msgFields := StringArray{}
+	indexPrefix := "logstash-2017.02.23"
 	timeField := "@timestamp"
 	include := ""
 	exclude := ""
 	size := 1000
-	poll := 1
+	//poll := 1
 	useSSL := false
-	useSource := false
-	showID := false
+	timeSpan := 30 * time.Second
 
 	flag.StringVar(&host, "host", host, "host and port of elasticsearch")
 	flag.StringVar(&indexPrefix, "prefix", indexPrefix, "prefix of log indexes")
-	flag.Var(&msgFields, "message", "message fields to display")
 	flag.StringVar(&timeField, "timestamp", timeField, "timestap field to sort by")
-	flag.StringVar(&include, "include", include, "comma separated list of field:value pairs to include")
-	flag.StringVar(&exclude, "exclude", exclude, "comma separated list of field:value pairs to exclude")
+	flag.StringVar(&include, "include", include, "DOESNT WORK: comma separated list of field:value pairs to include")
+	flag.StringVar(&exclude, "exclude", exclude, "DOESNT WORK: comma separated list of field:value pairs to exclude")
 	flag.IntVar(&size, "size", size, "number of docs to return per polling interval")
-	flag.IntVar(&poll, "poll", poll, "time in seconds to poll for new data from ES")
+	//flag.IntVar(&poll, "poll", poll, "time in seconds to poll for new data from ES")
 	flag.BoolVar(&useSSL, "ssl", useSSL, "use https for URI scheme")
-	flag.BoolVar(&useSource, "source", useSource, "use _source field to output result")
-	flag.BoolVar(&showID, "id", showID, "show _id field")
+	flag.DurationVar(&timeSpan, "dur", timeSpan, "now() - dur are how many logs are pulled")
 
 	flag.Parse()
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage of %s  --host=localhost:9401\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, `Usage of %s --host=localhost:9401 --dur=10s  # pull the latest 10 seconds of logs `, os.Args[0], os.Args[0])
 		flag.PrintDefaults()
+		return
 	}
 
 	logger := log.NewStdLogger(true, log.DEBUG, "")
-
-	// If no message field is explicitly requested we will follow @message
-	if len(msgFields) == 0 {
-		msgFields = append(msgFields, "@message")
-	}
-
-	exFilter := map[string]interface{}{}
-	if len(include) == 0 && len(exclude) == 0 {
-		exFilter["match_all"] = map[string]interface{}{}
-	} else {
-		filter := map[string]interface{}{}
-		if len(include) > 0 {
-			filter["must"] = getTerms(include)
-		}
-		if len(exclude) > 0 {
-			filter["must_not"] = getTerms(exclude)
-		}
-		exFilter["bool"] = filter
-	}
-
-	lastTime := time.Now()
 
 	var scheme string
 	if useSSL {
@@ -95,106 +57,51 @@ func main() {
 	}
 	rootURL := fmt.Sprintf("%s://%s", scheme, host)
 
-	for {
-		resp, err := http.Get(fmt.Sprintf("%s/_status", rootURL))
-		if err != nil {
-			fatalf("Error contacting Elasticsearch %s: %v", host, err)
+	docValueFilter := map[string]interface{}{}
+	if len(include) == 0 && len(exclude) == 0 {
+		docValueFilter["match_all"] = map[string]interface{}{}
+	} else {
+		filter := map[string]interface{}{}
+		if len(include) > 0 {
+			filter["must"] = getTerms(include)
 		}
-		status := map[string]interface{}{}
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fatalf("unable to read response body err:%v", err)
+		if len(exclude) > 0 {
+			filter["must_not"] = getTerms(exclude)
 		}
-		resp.Body.Close()
-		if err := json.Unmarshal(body, &status); err != nil {
-			fatalf("Error decoding _status response from Elasticsearch: err:%v json:%v", err, string(body))
-		}
-
-		logger.Debugf("idx resp: %v", string(body))
-
-		indices := []string{}
-		for k, _ := range status["indices"].(map[string]interface{}) {
-			if strings.HasPrefix(k, indexPrefix) {
-				indices = append(indices, k)
-			}
-		}
-		if len(indices) == 0 {
-			fatalf("No indexes found with the prefix '%s'", indexPrefix)
-		}
-		sort.Strings(indices)
-		index := indices[len(indices)-1]
-
-		url := fmt.Sprintf("%s/%s/_search", rootURL, index)
-		req, err := json.Marshal(map[string]interface{}{
-			"filter": map[string]interface{}{
-				"and": []interface{}{
-					map[string]interface{}{
-						"range": map[string]interface{}{
-							timeField: map[string]interface{}{
-								"gt": lastTime.Format(time.RFC3339Nano),
-							},
-						},
-					},
-					exFilter,
-				},
+		docValueFilter["bool"] = filter
+	}
+	timeRangeFilter := map[string]interface{}{
+		"range": map[string]interface{}{
+			timeField: map[string]interface{}{
+				"gt": time.Now().Add(-1 * timeSpan).Format(time.RFC3339Nano),
 			},
-			"size":    size,
-			"_source": useSource,
-			"fields":  append(msgFields, timeField),
-		})
+		},
+	}
+
+	filter := map[string]interface{}{
+		"and": []interface{}{
+			timeRangeFilter,
+			docValueFilter,
+		},
+	}
+
+	scanURL := fmt.Sprintf("%s/%s", rootURL, indexPrefix)
+	// Start the scroll first to make sure the source parameter is valid
+	ess := esscroll.New(context.Background(), scanURL, time.Minute, size, 3, filter, 10*time.Minute, logger)
+	resp, err := ess.Start()
+	if err != nil {
+		fatalf("%v", err)
+	}
+
+	b, _ := json.Marshal(filter)
+	logger.Infof("Scrolling over %d documents from %v : filter:%v\n", resp.Total, rootURL, string(b))
+
+	for doc := range resp.Hits {
+		b, err := doc.Source.MarshalJSON()
 		if err != nil {
-			fatalf("Error creating search body: %v", err)
+			fatalf("%v", err)
 		}
-		resp, err = http.Post(url, "application/json", bytes.NewReader(req))
-		if err != nil {
-			fatalf("Error searching Elasticsearch: %v", err)
-		}
-		if resp.StatusCode != 200 {
-			body, _ := ioutil.ReadAll(resp.Body)
-			fatalf("Elasticsearch failed: %s\n%s", resp.Status, string(body))
-		}
-		results := map[string]interface{}{}
-		if err := json.NewDecoder(resp.Body).Decode(&results); err != nil {
-			fatalf("Error reading search results: %v", err)
-		}
-		resp.Body.Close()
-
-		hits := results["hits"].(map[string]interface{})["hits"].([]interface{})
-		for _, hit := range hits {
-			h, ok := hit.(map[string]interface{})
-			if !ok {
-				continue
-			}
-
-			fields := h["fields"].(map[string]interface{})
-			var target map[string]interface{}
-			if useSource {
-				target = h["_source"].(map[string]interface{})
-			} else {
-				target = fields
-			}
-
-			output := []string{}
-			if showID {
-				output = append(output, fmt.Sprintf("id:%v", h["_id"]))
-			}
-
-			for _, msgField := range msgFields {
-				if v, _ := target[msgField]; v != nil {
-					output = append(output, fmt.Sprintf("%s:%v", msgField, v))
-				}
-			}
-
-			ts := fields[timeField].([]interface{})[0].(string)
-			fmt.Printf("%s %s\n", ts, strings.Join(output, "\t"))
-
-			lastTime, err = time.Parse(time.RFC3339Nano, ts)
-			if err != nil {
-				fatalf("Error decoding timestamp: %v", err)
-			}
-		}
-
-		time.Sleep(time.Duration(poll) * time.Second)
+		fmt.Printf("%s\n", string(b))
 	}
 }
 
@@ -211,6 +118,7 @@ func getTerms(args string) []map[string]interface{} {
 func parsePairs(args string) map[string][]string {
 	exkv := map[string][]string{}
 	for _, pair := range strings.Split(args, ",") {
+
 		kv := strings.Split(pair, ":")
 		if _, ok := exkv[kv[0]]; ok {
 			exkv[kv[0]] = append(exkv[kv[0]], kv[1])
